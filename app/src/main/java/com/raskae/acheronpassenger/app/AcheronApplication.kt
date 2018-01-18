3ce23b39c300c7f2f2374a1a15c1d5c629eae3f2
Pier Tenni <raskae@gmail.com>
2018-01-18 18:56:18 -0300

    added google firebase sign in. added empty application class. added dummy splash activity. starting to develop the main account list activity

---
+++ app/src/main/java/com/raskae/acheronpassenger/app/AcheronApplication.kt
@@ -0,0 +1,12 @@
+package com.raskae.acheronpassenger.app
+
+import android.app.Application
+
+/**
+ * Created by raskae on 1/18/18.
+ */
+class AcheronApplication: Application() {
+
+
+
+}
---
+++ app/src/main/java/com/raskae/acheronpassenger/app/accounts/list/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.raskae.acheronpassenger.app.accounts.list
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import com.raskae.acheronpassenger.R
+
+class MainActivity : AppCompatActivity() {
+
+    var accountsRecyclerView: RecyclerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //accountsRecyclerView = findViewById(R.id.rv_main_account_list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+}
