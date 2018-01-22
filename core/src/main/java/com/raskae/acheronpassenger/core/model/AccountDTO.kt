ba3c637b250db0e4e4ec5533a24484ea7378a8ec
Pier Tenni <raskae@gmail.com>
2018-01-22 17:33:51 -0300

    dtos

--- core/src/main/java/com/raskae/acheronpassenger/core/model/AccountDTO.kt
+++ core/src/main/java/com/raskae/acheronpassenger/core/model/AccountDTO.kt
@@ -3,6 +3,19 @@ package com.raskae.acheronpassenger.core.model
 /**
  * Created by Raskae on 18/01/2018.
  */
-data class AccountDTO(val alias: String, val login: String, val url: String) {
+data class AccountDTO(
+        val _id: String,
+        val alias: String,
+        val email: String,
+        val login: String,
+        val owner: String,
+        val password: String,
+        val status: String,
+        val tags: List<String>,
+        val url: String,
+        val owner_account: String, //raskae@gmail.com
+        val additional_data: List<AdditionalDataDTO>,
+        val coowners: List<String>
+) {
 
 }
